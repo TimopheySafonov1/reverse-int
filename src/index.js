b756a377042d27ee40266e545c25df5b25961483
Timophey.Safonov <earth2002ocean@gmail.com>
2023-09-17 17:34:41 +0200

    fix: delete extra spaces

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = 0;
+    let remainder = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        remainder = (n % 10);
+        result = (result * 10) + remainder;
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
